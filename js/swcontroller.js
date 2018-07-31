b11b50573533387f338884bbf543b0ec59f6d45b
Allen Salinas <allensalinas@outlook.es>
2018-07-31 18:50:15 -0500

    fix

--- js/swcontroller.js
+++ js/swcontroller.js
@@ -1,7 +1,7 @@
 if ('serviceWorker' in navigator) {
 
     console.log('sw habilitado en este navegador');
-    navigator.serviceWorker.register('/sw.js').then(function(reg) {
+    navigator.serviceWorker.register('./sw.js').then(function(reg) {
   
         if(reg.installing) {
           console.log('Service worker installing');
